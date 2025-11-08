bf6a15678c68d0515660d2252a978049c810f097
sidrashareef <sidrashareef99@gmail.com>
2025-11-07 23:09:06 -0500

    fixed tailwind setup, home template updated

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('home.twig');
